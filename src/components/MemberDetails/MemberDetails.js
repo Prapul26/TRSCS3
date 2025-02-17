9b11097df4c9dc78d9459b809bbfaf0b7a7d0eb9
Prapul <praul.pp@gmail.com>
2025-02-17 16:20:25 +0530

    17-02-25Update

--- src/components/MemberDetails/MemberDetails.js
+++ src/components/MemberDetails/MemberDetails.js
@@ -4,7 +4,7 @@ import { FaRegBuilding } from "react-icons/fa";
 import { FaPhoneAlt } from "react-icons/fa";
 import axios from "axios";
 
-import { IoMail, IoPersonSharp } from "react-icons/io5";
+import { IoLogoLinkedin, IoMail, IoPersonSharp } from "react-icons/io5";
 import { BsGlobe } from "react-icons/bs";
 import { FaUserTag } from "react-icons/fa6";
 import { FaRegCirclePlay } from "react-icons/fa6";
@@ -12,7 +12,7 @@ import { GrFormSchedule } from "react-icons/gr";
 import Header from "../Heaader/Header";
 import { FaLinkedinIn } from "react-icons/fa";
 import { RxCross2 } from "react-icons/rx";
-import { MdOutlineBusiness } from "react-icons/md"
+import { MdOutlineBusiness } from "react-icons/md";
 import { FaHeart } from "react-icons/fa6";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
@@ -39,41 +39,115 @@ const MemberDetails = () => {
       <Header />
       <Navbar />
       <div className="member-holder">
-        <div className="ProfilePic-holder">
-          <div className="backgroundPic">
-            <img src="https://images.pexels.com/photos/547114/pexels-photo-547114.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
-          </div>
-          <div className="userPic">
-            <img src="https://tracsdev.apttechsol.com/public/uploads/custom-images/user-2024-07-09-02-52-30-9248.png" />
-          </div>
+        <div className="userPic">
+          <img src="https://img.freepik.com/free-photo/indoor-shot-beautiful-happy-african-american-woman-smiling-cheerfully-keeping-her-arms-folded-relaxing-indoors-after-morning-lectures-university_273609-1270.jpg" />
         </div>
-        <div className="userNAME"><h2>Santhosh</h2></div>
-        <div  className="section2" style={{display:"flex"}}><div ><IoPersonSharp size={20}/></div><div style={{marginLeft:"10px",marginTop:"2px"}}><h3>Agent</h3></div> <div style={{marginLeft:"20px"}}><MdOutlineBusiness  size={20} /> </div><div style={{marginLeft:"10px",marginTop:"2px"}}><h3>Insurance Company</h3></div></div>
+
+        <div className="userNAME">
+          <h2 style={{ justifyContent: "center" }}>Santhosh</h2>
+        </div>
+        <div className="userNAME">
+          <h4 style={{ justifyContent: "center",marginTop:"-10px" }}>Agent</h4>
+        </div><div className="userNAME" >
+          <h4 style={{ justifyContent: "center",marginTop:"-10px" }}>Insurance Company</h4>
+        </div>
+
         <div className="divider">
- 
-        <div className="section3">
-          <div className="divider1">  <div className="EmailLink">
-        <div><IoMdMail /></div>
-        <div><h3>Email</h3></div></div>
-       <div className="PhoneLink">
-       <div><IoCall /></div>
-       <div><h3>987080979867</h3></div>
-       </div></div>
-     <div className="divider2"> <div className="WebsiteLink"> 
-       <div><TbArrowsRandom /></div>
-       <div><h3>Affiliation</h3></div> </div>
-       <div className="AffiliationLink">
-       <div><TbWorldWww /></div>
-       <div><h3>WebsiteLink</h3></div></div>
-      
-       </div>
-        </div>
+          <div className="section3">
+            <div className="divider1">
+              {" "}
+              <div className="infoheading">
+                <h3>Information</h3>
+              </div>
+              <div className="EmailLink">
+                <div className="mailIcon">
+                  <IoMdMail size={30} />
+                </div>
+                <div>
+                  <h4 style={{ textAlign: "left" }}>Email</h4>
+                  <h3>sample@mail.com</h3>
+                </div>
+              </div>
+              <div className="PhoneLink">
+                <div className="PhoneIcon">
+                  <IoCall size={30} />
+                </div>
+                <div>
+                  <h4 style={{ textAlign: "left" }}>Phone</h4>
+                  <h3>987080979867</h3>
+                </div>
+              </div>
+            </div>
+            <div className="divider2">
+              <div className="infoheading">
+                <h3 style={{ color: "white" }}>.</h3>
+              </div>{" "}
+              <div className="WebsiteLink">
+                <div className="randomIcon">
+                  <TbArrowsRandom size={30} />
+                </div>
+                <div>
+                  <h4 style={{ textAlign: "left" }}>Affiliation</h4>
+                  <h3>http/wdad/wsampke</h3>
+                </div>{" "}
+              </div>
+              <div className="AffiliationLink">
+                <div className="webIcon">
+                  <TbWorldWww size={30} />
+                </div>
+                <div style={{ textAlign: "center" }}>
+                  <h4 style={{ textAlign: "left" }}> WebsiteLink</h4>
+                   <h3>www.sampleWeblink.com</h3>
+                </div>
+              </div>
+              <div className="AffiliationLink">
+                <div className="webIcon">
+                  <IoLogoLinkedin size={30} />
+                </div>
+                <div style={{ textAlign: "center" }}>
+                  <h4 style={{ textAlign: "left" }}> Linked In</h4>
+                  <h3>www.LinkedInSample.com</h3>
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="About-Container">
-          <div><h2>About Us:</h2></div>
-          <div><p>At TRACS, we believe in the power of connections. Our platform is designed to be the heartbeat of professional networking, where members come together to forge meaningful relationships, exchange ideas, and unlock new opportunities. Our platform is designed for individuals and businesses seeking meaningful networking experiences.</p></div>
-        </div></div>
-       
-       
+            <div>
+              <h2>About Us:</h2>
+            </div>
+            <div>
+              <p>
+                At TRACS, we believe in the power of connections. Our platform
+                is designed to be the heartbeat of professional networking,
+                where members come together to forge meaningful relationships,
+                exchange ideas, and unlock new opportunities. Our platform is
+                designed for individuals and businesses seeking meaningful
+                networking experiences.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div style={{ marginTop: "20px", marginLeft: "20px" }}>
+          <h3>Images</h3>
+        </div>
+        <div className="imagesAdd">
+          <div className="imageAdd1">
+            <div>
+              <img src="https://static.vecteezy.com/system/resources/thumbnails/007/209/020/small_2x/close-up-shot-of-happy-dark-skinned-afro-american-woman-laughs-positively-being-in-good-mood-dressed-in-black-casual-clothes-isolated-on-grey-background-human-emotions-and-feeligs-concept-photo.jpg" />
+            </div>
+            <div>
+              <img src="https://wallpapercave.com/wp/wp2656149.jpg" />
+            </div>
+          </div>
+          <div className="imageAdd2">
+            <div>
+              <img src="https://pbs.twimg.com/profile_images/1442802623338397702/xcIJg8PN_400x400.jpg" />
+            </div>
+            <div>
+              <img src="https://th.bing.com/th/id/OIP.z9lqZb1IAWlS5YPDLMHd4QDMEy?w=400&h=600&rs=1&pid=ImgDetMain" />
+            </div>
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
