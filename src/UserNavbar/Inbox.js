bc1d83615d63843a6d5691c918d7ae1cf55e8fe8
Prapul <praul.pp@gmail.com>
2025-03-10 19:02:00 +0530

    10-03-25Commit

--- src/UserNavbar/Inbox.js
+++ src/UserNavbar/Inbox.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Inbox.css";
+import "./NewInbox.css";
 import { RiContactsBook3Line } from "react-icons/ri";
 import { IoMdPerson } from "react-icons/io";
 import UserHeader from "../components/UserHeader";
 import { FaFileSignature } from "react-icons/fa";
 import { FaHome } from "react-icons/fa";
 import { SlLogout } from "react-icons/sl";
-import { MdOutlineCardMembership } from "react-icons/md";
+import { MdArrowCircleDown, MdOutlineCardMembership } from "react-icons/md";
 import { FaBriefcase } from "react-icons/fa6";
 import { IoSettingsSharp } from "react-icons/io5";
 import { ImProfile } from "react-icons/im";
@@ -16,6 +17,8 @@ import { RiContactsFill } from "react-icons/ri";
 import { HiInboxArrowDown } from "react-icons/hi2";
 import { IoBookOutline } from "react-icons/io5";
 import { MdOutlineEmail } from "react-icons/md";
+import { IoExpandSharp } from "react-icons/io5";
+
 import { MdPerson } from "react-icons/md";
 import { FaPhoneAlt } from "react-icons/fa";
 import { FaLock } from "react-icons/fa6";
@@ -26,25 +29,37 @@ import { IoPerson } from "react-icons/io5";
 import { useState } from "react";
 import MobileNavbar from "../components/MobileNavbar/MobileNavbar";
 import SideNav from "./SideNav";
+import inboxData from "../components/Data/inboxData";
 const Inbox = () => {
   const [intro, showIntro] = useState(false);
   const [settings, showSettings] = useState(false);
+  const [data2, setData] = useState(false);
+  const handelExpand = () => {
+    setData(!data2);
+  };
   const handelSettings = () => {
     showSettings(!settings);
   };
   const handelIntro = () => {
     showIntro(!intro);
   };
+  const [expandedIndex, setExpandedIndex] = useState(null);
+
+  // Toggle function for expanding the clicked item
+  const handleExpand = (index) => {
+    setExpandedIndex(expandedIndex === index ? null : index);
+  };
   return (
     <div>
       {" "}
       <UserHeader />
-     
       <div className="IBPPP">
         <div className="usernav">
           <SideNav />
         </div>
-        <div className="IBPP"> <MobileNavbar />
+        <div className="IBPP">
+          {" "}
+          <MobileNavbar />
           <div className="c-header">
             <div className="c-h1">
               <h3>Inbox</h3>
@@ -67,23 +82,94 @@ const Inbox = () => {
           </div>
           <div className="inbox-container">
             <div className="messageInbox">
-            <div className="mI1">
-              <p>Filter for Replies</p>
-              <select>
-                <option value="allReplies">All Replies</option>
-                <option value="noReplies">No Replies(Bump)</option>
-              </select>
+              <div className="mI1">
+                <p>Filter for Replies</p>
+                <select>
+                  <option value="allReplies">All Replies</option>
+                  <option value="noReplies">No Replies(Bump)</option>
+                </select>
+              </div>
+              <div className="mI2">
+                <p>Filter for Messages</p>
+                <select>
+                  <option value="allReplies">All </option>
+                  <option value="noReplies">Intros Recived</option>
+                  <option value="noReplies">Intros Sent</option>
+                </select>
+              </div>
+
+              <div className="mI3">
+                <Link to="/makeIntro">
+                  <button>Make Introduction</button>
+                </Link>{" "}
+              </div>
+            </div>   <div className="mailInbox-Mobile">
+              {inboxData.map((data, index) => (
+                <div className="mobileInbox-Holder" key={index}>
+                  <div className="mobileInboxData">
+                    <div className="DandN">
+                    <div className="name">
+                        <h2>Introduction : {data.introduction}</h2>
+                      </div>
+                      <div className="date">{data.date}</div>
+                      <div className="name">
+                        <h2 >{data.recipient}</h2>
+                      </div>
+                    </div>
+                    <div
+                      className="expand"
+                      onClick={() => handleExpand(index)}
+                    >
+                      <IoExpandSharp  size={20} />
+                    </div>
+                  </div>
+
+                  {expandedIndex === index && (
+                    <div className="expanded-view">
+                      <Link to="/memberDetails" style={{ color: "inherit" }}>
+                        <div className="messageTo">
+                          <div style={{ display: "flex", flexDirection: "row" }}>
+                            <div
+                              style={{
+                                marginTop: "17px",
+                                marginRight: "5px",
+                              }}
+                            >
+                              <IoMdPerson />
+                            </div>
+                            <p>
+                              {data.sender.name} (Replies:{" "}
+                              {data.sender.replies})
+                            </p>
+                          </div>
+                          <div style={{ display: "flex", flexDirection: "row" }}>
+                            <div
+                              style={{
+                                marginTop: "17px",
+                                marginRight: "5px",
+                              }}
+                            >
+                              <RiContactsBook3Line />
+                            </div>
+                            <p>{data.sender.name2}</p>
+                          </div>
+                        </div>
+                      </Link>
+                      <div className="messageAction">
+                        <div>
+                          <Link to="/reply">
+                            <button style={{background:"green"}}> Reply</button>
+                          </Link>
+                        </div>
+                        <div>
+                          <button style={{ marginLeft: "10px" ,background:"red"}}>Bump</button>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              ))}
             </div>
-            <div className="mI2">
-              <p>Filter for Messages</p>
-              <select>
-                <option value="allReplies">All </option>
-                <option value="noReplies">Intros Recived</option>
-                <option value="noReplies">Intros Sent</option>
-              </select>
-            </div>
-          
-             <div className="mI3"><Link to='/makeIntro'><button>Make Introduction</button></Link> </div></div>
             <div className="inbox-holder">
               <table>
                 <thead>
@@ -102,23 +188,28 @@ const Inbox = () => {
                     </td>
 
                     <td>
-                    <Link to='/reply'><button style={{ background: "green" }}>Reply</button></Link> 
+                      <Link to="/reply">
+                        <button style={{ background: "green" }}>Reply</button>
+                      </Link>
                       <button style={{ background: "pink" }}>Bump</button>
                     </td>
-                 <Link to='/memberDetails'>   <td>
-                      <p>
-                        <IoMdPerson />
-                        Santhosh Kumar (Replies:0)
-                      </p>
-                      <p>
-                        <IoMdPerson />
-                        Skumar nelli(Replies:0)
-                      </p>
-                      <p>
-                        <RiContactsBook3Line />
-                        narendhar Kumar
-                      </p>
-                    </td></Link>
+                    <Link to="/memberDetails">
+                      {" "}
+                      <td>
+                        <p>
+                          <IoMdPerson />
+                          Santhosh Kumar (Replies:0)
+                        </p>
+                        <p>
+                          <IoMdPerson />
+                          Skumar nelli(Replies:0)
+                        </p>
+                        <p>
+                          <RiContactsBook3Line />
+                          narendhar Kumar
+                        </p>
+                      </td>
+                    </Link>
                   </tr>
                   <tr>
                     <td>
@@ -128,23 +219,29 @@ const Inbox = () => {
                     </td>
 
                     <td>
-                    <Link to='/reply'> <button style={{ background: "green" }}>Reply</button></Link>
+                      <Link to="/reply">
+                        {" "}
+                        <button style={{ background: "green" }}>Reply</button>
+                      </Link>
                       <button style={{ background: "pink" }}>Bump</button>
                     </td>
-                  <Link to='/memberDetails'>  <td>
-                      <p>
-                        <IoMdPerson />
-                        Santhosh Kumar (Replies:0)
-                      </p>
-                      <p>
-                        <IoMdPerson />
-                        Skumar nelli(Replies:0)
-                      </p>
-                      <p>
-                        <RiContactsBook3Line />
-                        narendhar Kumar
-                      </p>
-                    </td></Link>
+                    <Link to="/memberDetails">
+                      {" "}
+                      <td>
+                        <p>
+                          <IoMdPerson />
+                          Santhosh Kumar (Replies:0)
+                        </p>
+                        <p>
+                          <IoMdPerson />
+                          Skumar nelli(Replies:0)
+                        </p>
+                        <p>
+                          <RiContactsBook3Line />
+                          narendhar Kumar
+                        </p>
+                      </td>
+                    </Link>
                   </tr>
                   <tr>
                     <td>
@@ -154,23 +251,28 @@ const Inbox = () => {
                     </td>
 
                     <td>
-                    <Link to='/reply'><button style={{ background: "green" }}>Reply</button></Link> 
+                      <Link to="/reply">
+                        <button style={{ background: "green" }}>Reply</button>
+                      </Link>
                       <button style={{ background: "pink" }}>Bump</button>
                     </td>
-                   <Link to='/memberDetails'> <td>
-                      <p>
-                        <IoMdPerson />
-                        Santhosh Kumar (Replies:0)
-                      </p>
-                      <p>
-                        <IoMdPerson />
-                        Skumar nelli(Replies:0)
-                      </p>
-                      <p>
-                        <RiContactsBook3Line />
-                        narendhar Kumar
-                      </p>
-                    </td></Link>
+                    <Link to="/memberDetails">
+                      {" "}
+                      <td>
+                        <p>
+                          <IoMdPerson />
+                          Santhosh Kumar (Replies:0)
+                        </p>
+                        <p>
+                          <IoMdPerson />
+                          Skumar nelli(Replies:0)
+                        </p>
+                        <p>
+                          <RiContactsBook3Line />
+                          narendhar Kumar
+                        </p>
+                      </td>
+                    </Link>
                   </tr>
                   <tr>
                     <td>
@@ -180,23 +282,29 @@ const Inbox = () => {
                     </td>
 
                     <td>
-                    <Link to='/reply'> <button style={{ background: "green" }}>Reply</button></Link>
+                      <Link to="/reply">
+                        {" "}
+                        <button style={{ background: "green" }}>Reply</button>
+                      </Link>
                       <button style={{ background: "pink" }}>Bump</button>
                     </td>
-                  <Link to='/memberDetails'>  <td>
-                      <p>
-                        <IoMdPerson />
-                        Santhosh Kumar (Replies:0)
-                      </p>
-                      <p>
-                        <IoMdPerson />
-                        Skumar nelli(Replies:0)
-                      </p>
-                      <p>
-                        <RiContactsBook3Line />
-                        narendhar Kumar
-                      </p>
-                    </td></Link>
+                    <Link to="/memberDetails">
+                      {" "}
+                      <td>
+                        <p>
+                          <IoMdPerson />
+                          Santhosh Kumar (Replies:0)
+                        </p>
+                        <p>
+                          <IoMdPerson />
+                          Skumar nelli(Replies:0)
+                        </p>
+                        <p>
+                          <RiContactsBook3Line />
+                          narendhar Kumar
+                        </p>
+                      </td>
+                    </Link>
                   </tr>
                   <tr>
                     <td>
@@ -206,23 +314,29 @@ const Inbox = () => {
                     </td>
 
                     <td>
-                    <Link to='/reply'>   <button style={{ background: "green" }}>Reply</button></Link>
+                      <Link to="/reply">
+                        {" "}
+                        <button style={{ background: "green" }}>Reply</button>
+                      </Link>
                       <button style={{ background: "pink" }}>Bump</button>
                     </td>
-                 <Link to='/memberDetails'>  <td>
-                      <p>
-                        <IoMdPerson />
-                        Santhosh Kumar (Replies:0)
-                      </p>
-                      <p>
-                        <IoMdPerson />
-                        Skumar nelli(Replies:0)
-                      </p>
-                      <p>
-                        <RiContactsBook3Line />
-                        narendhar Kumar
-                      </p>
-                    </td></Link> 
+                    <Link to="/memberDetails">
+                      {" "}
+                      <td>
+                        <p>
+                          <IoMdPerson />
+                          Santhosh Kumar (Replies:0)
+                        </p>
+                        <p>
+                          <IoMdPerson />
+                          Skumar nelli(Replies:0)
+                        </p>
+                        <p>
+                          <RiContactsBook3Line />
+                          narendhar Kumar
+                        </p>
+                      </td>
+                    </Link>
                   </tr>
                   <tr>
                     <td>
@@ -232,23 +346,28 @@ const Inbox = () => {
                     </td>
 
                     <td>
-                     <Link to='/reply'><button style={{ background: "green" }}>Reply</button></Link> 
+                      <Link to="/reply">
+                        <button style={{ background: "green" }}>Reply</button>
+                      </Link>
                       <button style={{ background: "pink" }}>Bump</button>
                     </td>
-                  <Link to='/memberDetails'>  <td>
-                      <p>
-                        <IoMdPerson />
-                        Santhosh Kumar (Replies:0)
-                      </p>
-                      <p>
-                        <IoMdPerson />
-                        Skumar nelli(Replies:0)
-                      </p>
-                      <p>
-                        <RiContactsBook3Line />
-                        narendhar Kumar
-                      </p>
-                    </td></Link>
+                    <Link to="/memberDetails">
+                      {" "}
+                      <td>
+                        <p>
+                          <IoMdPerson />
+                          Santhosh Kumar (Replies:0)
+                        </p>
+                        <p>
+                          <IoMdPerson />
+                          Skumar nelli(Replies:0)
+                        </p>
+                        <p>
+                          <RiContactsBook3Line />
+                          narendhar Kumar
+                        </p>
+                      </td>
+                    </Link>
                   </tr>
                 </tbody>
               </table>
---
+++ src/components/Data/inboxData.js
@@ -0,0 +1,105 @@
+const inboxData = [
+    {
+      id: 1,
+      date: "March,10 2025 08:42am",
+      introduction:"Manohar Porthi",
+      sender: {
+        name: "manohar porthi",
+        
+        replies: 0,
+      },
+      recipient: "S Kumar Nelli",
+    },
+    {
+      id: 2,
+      date: "March,05 2025 09:42am",
+      introduction:"Swaran jeeth and Shankar Vanga ",
+      sender: {
+        name: "Swaran Jeeth",
+        name2: " Shankar Vanga",
+        replies: 3,
+      },
+      recipient: "S Kumar Nelli",
+    },
+    {
+      id: 3,
+      date: "March,6 2025 08:42am",
+      introduction:"S Kumar Nelli and  sample user ",
+      sender: {
+        name: "S Kumar Nelli",
+
+        replies: 1,
+      },
+      recipient: "S Kumar Nelli",
+    },
+    {
+        id: 1,
+        date: "March,03 2025 08:42pm",
+        introduction:"abxxyz and manohar porthi",
+        sender: {
+          name: "manohar porthi",
+          
+          replies: 0,
+        },
+        recipient: "S Kumar Nelli",
+      },
+      {
+        id: 2,
+        date: "March,04 2025 10:42pm",
+        introduction:"manohar porthi and Shankar Vanga",
+        sender: {
+          name: "manohar porthi",
+          name2: "Shankar vanga",
+          replies: 3,
+        },
+        recipient: "S Kumar Nelli",
+      },
+      {
+        id: 3,
+        date: "March,01 2025 03:42am",
+        introduction:"Aaron Saltzman and trevor-1 Agre",
+        sender: {
+          name: "Aaron Saltzman",
+          name2: " Trevor-1 Agre",
+          replies: 1,
+        },
+        recipient: "S Kumar Nelli",
+      },
+      {
+        id: 1,
+        date: "March,04 2025 08:40 am",
+        introduction:"Arazon Saltzman and Sample user and Trevor-1 Agre and Aaron Saltzman",
+        sender: {
+          name: "Aaron Saltzman",
+          name2: " Trevor-1 Agre",
+          name3:"Aaron Saltzman",
+          replies: 0,
+        },
+        recipient: "Narendhar Kumar",
+      },
+      {
+        id: 2,
+        date: "March,04 2025 05:50 am",
+        introduction:"Trevor-1 Agre and srini d",
+                sender: {
+          name: "John",
+          name2: "sini D",
+          replies: 3,
+        },
+        recipient: "S kumar Nelli",
+      },
+      {
+        id: 3,
+           date: "January,21 2025 11:45 am",
+        introduction:"Kumar Nelli and shvanga Vanga",
+        sender: {
+          name: "Trevor-1 Agre",
+          name2: "Shankar Vanga",
+          replies: 1,
+        },
+        recipient: "Shankar Vanga",
+      },
+  ];
+  
+  export default inboxData;
+  
