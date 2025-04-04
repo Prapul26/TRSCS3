5ed8f577ddea325a6d4ca2f3b32799488e9ef43c
Prapul <praul.pp@gmail.com>
2025-04-04 11:57:32 +0530

    04-04-25Commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import './Footer.css'
 import gplay from '../../assets/ggplay.jpg'
-import { FaFacebook } from "react-icons/fa";
+import { FaFacebook, FaPhone, FaPhoneAlt } from "react-icons/fa";
 
-import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaPhoneFlip, FaSquareXTwitter } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa6";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaLinkedinIn } from "react-icons/fa";
 import aplay from '../../assets/apple.png'
 import { Link } from 'react-router-dom';
+import { CiLocationOn } from 'react-icons/ci';
+import { IoMagnetOutline, IoMailOutline } from 'react-icons/io5';
 const Footer = () => {
     return (
         <div className='footer-container'>
@@ -16,25 +18,35 @@ const Footer = () => {
             <div className='dlc-holder'>
                 <div className='download-container'>
                     <h1>Download Our App</h1>
+                    <div className='store-holder' style={{display:"flex"}}>
                     <div className='playStore-container'>
-                        <a href="https://play.google.com/store/apps/details?id=com.hotspot.tracs&pli=1" target="_blank" rel="noopener noreferrer"><img src={gplay} /></a>
+                        <a href="https://play.google.com/store/apps/details?id=com.hotspot.tracs&pli=1" target="_blank" rel="noopener noreferrer"><img src="https://tracs.app/public/user/images/google-play-store.png" /></a>
                     </div>
                     <div className='appleStore-container'>
-                        <a href='https://apps.apple.com/us/app/tracs-for-h7-network/id6446471421' target="_blank" rel="noopener noreferrer"><img src={aplay} /></a>
+                        <a href='https://apps.apple.com/us/app/tracs-for-h7-network/id6446471421' target="_blank" rel="noopener noreferrer"><img src="https://tracs.app/public/user/images/apple-store.png" /></a>
 
+                    </div>
+                    
+                    </div>
+                    <div className='socialInvisible'>
+                    <a href="https://www.facebook.com/h7network"><div className="facebook">
+                                    {" "}
+                                    <FaFacebook size={28} color='black'/>
+                                  </div></a> 
+                                
+                                 <a href="https://www.linkedin.com/company/h7network/"><div className="LinkedIn">
+                                    <FaLinkedin size={28} color="black" />
+                                  </div></a> 
                     </div>
                     <div className='socilaMediaStore-container'>
                          <div className="socila-icons">
                                  <a href="https://www.facebook.com/h7network"><div className="facebook">
                                     {" "}
-                                    <FaFacebook size={28} />
+                                    <FaFacebook size={28} color='black'/>
                                   </div></a> 
-                                 <a href="https://twitter.com"><div className="x">
-                                    {" "}
-                                    <FaSquareXTwitter size={28} />
-                                  </div></a> 
+                                
                                  <a href="https://www.linkedin.com/company/h7network/"><div className="LinkedIn">
-                                    <FaLinkedin size={28} />
+                                    <FaLinkedin size={28} color="black" />
                                   </div></a> 
                                 </div>
                     </div>
@@ -47,24 +59,34 @@ const Footer = () => {
                       <Link to='/home' style={{textDecoration:"none",color:"inherit"}}> <p style={{color:"black"}}>Home</p></Link> 
                       <Link to='/pricing' style={{textDecoration:"none",color:"inherit"}}> <p style={{color:"black"}}>Pricing</p></Link> 
                       <Link to='/about_us' style={{textDecoration:"none",color:"inherit"}}><p style={{color:"black"}}>About Us</p></Link>  
-                      <Link to='/blog' style={{textDecoration:"none",color:"inherit"}}> <p style={{color:"black"}}>Blog</p></Link>
+                     
 
                     </div>
                 </div>
                 <div className='contact-container'>
                     <div className='contact'>
                         <h1>Contact Us</h1>
-                        <div style={{ borderBottom: "1px solid #9a9a9a " }}><ul style={{ display: "flex", flexDirection: "column", flexWrap: "wrap",textDecoration:"none" }}>
-                            <li style={{listStyle:"none"}}>4031 Colonel Glenn Hwy Suite 416</li>
-                               <li style={{listStyle:"none"}}> Dayton ,OH 45431</li></ul></div>
+                        <div  className="conulli"style={{ display:"flex"}}>
+                        <div><CiLocationOn size={20} /> </div>
+                           <div style={{marginTop:"-18px"}}> <ul style={{ display: "flex", flexDirection: "column", flexWrap: "wrap",textDecoration:"none" }}>
+                         <li style={{listStyle:"none"}}>4031 Colonel Glenn Hwy Suite 416</li>
+                               <li style={{listStyle:"none"}}> Dayton ,OH 45431</li></ul>
+                               </div>
+                               
+                               </div>
 
 
-                        <div style={{ borderBottom: "1px solid #9a9a9a " }}><ul><li style={{listStyle:"none"}}>support@tracs.app</li>
-                        </ul></div>
-                        <div >
-                            <ul>
-                                <li style={{listStyle:"none"}}>513.371.5299</li>
-                            </ul>
+                        <div className="conulli" style={{ display:"flex"}}>
+                            <div><IoMailOutline /></div>
+                        <div style={{marginTop:"-18px"}}>   <ul><li style={{listStyle:"none"}}> support@tracs.app</li>
+                        </ul>
+                        </div> 
+                        </div>
+                        <div  className="conulli" style={{display:"flex"}}> <div style={{marginTop:"0px"}}><FaPhoneAlt /></div>
+                          <div style={{marginTop:"-16px"}}> <ul>
+                                <li style={{listStyle:"none"}}> 513.371.5299</li>
+                            </ul></div> 
+                           
                         </div>
 
 
