58bf4e3900ffe334ebff2fa4d79d99d9249d0743
Prapul Kumar <prapul.pp@gmail.com>
2025-08-28 18:47:15 +0530

    implemented faq api in faq page

--- src/components/Faq/Faq.js
+++ src/components/Faq/Faq.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Faq.css';
 import Header from '../Heaader/Header';
 import Navbar from '../Navbar/Navbar';
@@ -7,7 +7,31 @@ import Footer from '../Footer/Footer';
 import { RiArrowRightSLine } from 'react-icons/ri';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { FaMinus, FaPlus } from 'react-icons/fa';
 const Faq = () => {
+  const[data,setData]=useState([]);
+const [showDetails,setDetails]=useState(null);
+const detailsHandle=(id)=>{
+  setDetails(previd => previd === id ? null : id)
+}
+const adjustInternalHtml=(html)=>{
+  const container=document.createElement("div");
+  container.innerHTML=html;
+  return container.innerHTML;
+}
+useEffect(()=>{
+  const fetchData=async()=>{
+
+  
+  try{
+    const response=await axios.get("https://tracsdev.apttechsol.com/api/Faqsindex");
+    setData(response.data.faqs);
+console.log("faqs :"+response.data?.faqs[1])
+  }catch(err){
+    console.log(err)
+  }}
+fetchData();},[]);
+
   return (
   <div className="homeHolder">
       <Header />
@@ -18,7 +42,13 @@ const Faq = () => {
           </div>
         </div>    
         <div className='faqContainer'>
-          
+          {
+            data.map((faq,index)=>(
+              <div>
+              <div key={faq.id} className='faqHolder' onClick={()=>detailsHandle(faq.id)}><div><strong style={{fontWeight:"600"}}>{faq.name}</strong></div><div>{ showDetails===faq.id ? <FaMinus />:<FaPlus />}</div></div>
+             <div> {showDetails===faq.id && <div className='faqDes'><p> <div dangerouslySetInnerHTML={{__html: adjustInternalHtml(faq.description)}}></div></p></div>}</div></div>
+            ))
+          }
             </div> 
       <Footer />
     </div>
--- src/components/Help/Help.js
+++ src/components/Help/Help.js
@@ -8,41 +8,43 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
 const Help = () => {
-const[data ,setData]=useState([]);
-const [activeId, setActiveId] = useState(null);
-const [id,setId]=useState(null);
-const toggleDropDown = (id) => {
-  setActiveId(prevId => prevId === id ? null : id);
-  setId(id);
-};
+  const [data, setData] = useState([]);
+  const [activeId, setActiveId] = useState(null);
+  const [id, setId] = useState(null);
+  const toggleDropDown = (id) => {
+    setActiveId(prevId => prevId === id ? null : id);
+    setId(id);
+  };
 
-const[data2,setData2]=useState([]);
+  const [data2, setData2] = useState([]);
 
-useEffect(()=>{
-const fetchData2=async()=>{
-try{
+  useEffect(() => {
+    const fetchData2 = async () => {
+      try {
 
-const response =await axios.get(`https://tracsdev.apttechsol.com/api/helpsection/${id}`);
-setData2(response.data.subtitles)
-}catch(err){
-console.log(err)
-}
-};
-fetchData2()},[id])
+        const response = await axios.get(`https://tracsdev.apttechsol.com/api/helpsection/${id}`);
+        setData2(response.data.subtitles)
+      } catch (err) {
+        console.log(err)
+      }
+    };
+    fetchData2()
+  }, [id])
 
 
 
 
-  useEffect(()=>{
-const fetchData=async()=>{
-  try{
-const response=await axios.get("https://tracsdev.apttechsol.com/api/helpsection ");
-setData(response.data.blogs.data || [])
-  }catch(err){
-    console.log(err)
-  }
-}
-fetchData();  },)
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://tracsdev.apttechsol.com/api/helpsection ");
+        setData(response.data.blogs.data || [])
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    fetchData();
+  },)
   return (
     <div className="homeHolder">
       <Header />
@@ -50,34 +52,34 @@ fetchData();  },)
 
       <div className='helpcurb1'><p style={{ color: "#007bff !important" }} className='ppffg'></p><p></p><p style={{ marginLeft: '10px' }}>Help</p></div>
       <div className='helpContainer'>
- {data.map((help, index) => (
-  <div>
-          <div className='makeIntoButtonContainer' onClick={() => toggleDropDown(help.id)} key={help.id}>
-            <div>
-              <h5>
-                
+        {data.map((help, index) => (
+          <div>
+            <div className='makeIntoButtonContainer' onClick={() => toggleDropDown(help.id)} key={help.id}>
+              <div>
+                <h5>
+
                   {help.title}
-               
-              </h5>
-            </div>
-            <div>
-             
-              <h5 style={{ fontWeight: '800', color: "#007bff" }}>{ activeId===help.id ?<FaChevronUp />:<FaChevronDown />}</h5>
-            </div>
-         
-          </div>   {activeId === help.id && (
-      <div className='subtitles'>
-        {data2.map((help,index)=>( <Link to={`/helpDescription/${help.id}`} style={{textDecoration:"none",color:"inherit"}}><div style={{marginBottom:"30px"}} className='titles' key={help.id}>
-                            <div>
-                             <h5>{help.title}</h5></div>
-                            <div>
-                              <h5 style={{ fontWeight: '800', color: "#007bff" }}>{">"}</h5></div>
-                        </div></Link> ))}
-      </div>
-    )} </div>
+
+                </h5>
+              </div>
+              <div>
+
+                <h5 style={{ fontWeight: '800', color: "#007bff" }}>{activeId === help.id ? <FaChevronUp /> : <FaChevronDown />}</h5>
+              </div>
+
+            </div>   {activeId === help.id && (
+              <div className='subtitles'>
+                {data2.map((help, index) => (<Link to={`/helpDescription/${help.id}`} style={{ textDecoration: "none", color: "inherit" }}><div style={{ marginBottom: "30px" }} className='titles' key={help.id}>
+                  <div>
+                    <h5>{help.title}</h5></div>
+                  <div>
+                    <h5 style={{ fontWeight: '800', color: "#007bff" }}>{">"}</h5></div>
+                </div></Link>))}
+              </div>
+            )} </div>
         ))}
-       
-        
+
+
         <div></div>
         <div></div>
       </div>
