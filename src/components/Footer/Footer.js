07009ee4e4883c02b76d9882f0dd4b8d19b4807b
Prapul Kumar <prapul.pp@gmail.com>
2025-09-19 14:41:56 +0530

    footer spacing  fixed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -18,72 +18,72 @@ const Footer = () => {
             <div className='dlc-holder'>
                 <div className='download-container'>
                     <h2>Download Our App</h2>
-                    <div className='store-holder' style={{display:"flex"}}>
-                    <div className='playStore-container'>
-                        <a href="https://play.google.com/store/apps/details?id=com.hotspot.tracs&pli=1" target="_blank" rel="noopener noreferrer"><img src="https://tracs.app/public/user/images/google-play-store.png" /></a>
-                    </div>
-                    <div className='appleStore-container'>
-                        <a href='https://apps.apple.com/us/app/tracs-for-h7-network/id6446471421' target="_blank" rel="noopener noreferrer"><img src="https://tracs.app/public/user/images/apple-store.png" /></a>
+                    <div className='store-holder' style={{ display: "flex" }}>
+                        <div className='playStore-container'>
+                            <a href="https://play.google.com/store/apps/details?id=com.hotspot.tracs&pli=1" target="_blank" rel="noopener noreferrer"><img src="https://tracs.app/public/user/images/google-play-store.png" /></a>
+                        </div>
+                        <div className='appleStore-container'>
+                            <a href='https://apps.apple.com/us/app/tracs-for-h7-network/id6446471421' target="_blank" rel="noopener noreferrer"><img src="https://tracs.app/public/user/images/apple-store.png" /></a>
+
+                        </div>
 
                     </div>
-                    
-                    </div>
                     <div className='socialInvisible'>
-                    <a href="https://www.facebook.com/h7network"><div className="facebook">
-                                    {" "}
-                                    <FaFacebook size={28} color='black'/>
-                                  </div></a> 
-                                
-                                 <a href="https://www.linkedin.com/company/h7network/"><div className="LinkedIn">
-                                    <FaLinkedin size={28} color="black" />
-                                  </div></a> 
+                        <a href="https://www.facebook.com/h7network"><div className="facebook">
+                            {" "}
+                            <FaFacebook size={28} color='black' />
+                        </div></a>
+
+                        <a href="https://www.linkedin.com/company/h7network/"><div className="LinkedIn">
+                            <FaLinkedin size={28} color="black" />
+                        </div></a>
                     </div>
                     <div className='socilaMediaStore-container'>
-                         <div className="socila-icons">
-                                 <a href="https://www.facebook.com/h7network" target="_blank" 
-  rel="noopener noreferrer"><div className="facebook">
+                        <div className="socila-icons">
+                            <a href="https://www.facebook.com/h7network" target="_blank"
+                                rel="noopener noreferrer"><div className="facebook">
                                     {" "}
-                                    <FaFacebook size={28} color='black'/>
-                                  </div></a> 
-                                
-                                 <a href="https://www.linkedin.com/company/h7network/" target="_blank" 
-  rel="noopener noreferrer"><div className="LinkedIn">
+                                    <FaFacebook size={28} color='black' />
+                                </div></a>
+
+                            <a href="https://www.linkedin.com/company/h7network/" target="_blank"
+                                rel="noopener noreferrer"><div className="LinkedIn">
                                     <FaLinkedin size={28} color="black" />
-                                  </div></a> 
-                                </div>
+                                </div></a>
+                        </div>
                     </div>
                 </div>
                 <div className='important-links-container'>
                     <div className='imp-links'>
-                       
+
                     </div>
-                    <div className='wdadad'><a href="https://h7network.blog/" target="_blank" 
-  rel="noopener noreferrer" style={{color:"inherit",textDecoration:"none"}}><p>TRACS is a subsidiary of the H7 Network</p></a></div>
+                    <div className='wdadad'><a href="https://h7network.blog/" target="_blank"
+                        rel="noopener noreferrer" style={{ color: "inherit", textDecoration: "none" }}><p>TRACS is a subsidiary of the H7 Network</p></a></div>
                 </div>
                 <div className='contact-container'>
                     <div className='contact'>
-                      
-                        <div  className="conulli"style={{ display:"flex"}}>
-                        <div><CiLocationOn size={20} /> </div>
-                           <div style={{marginTop:"-18px"}}> <ul style={{ display: "flex", flexDirection: "column", flexWrap: "wrap",textDecoration:"none" }}>
-                         <li style={{listStyle:"none",fontWeight:"400",fontSize:"16px"}}>4031 Colonel Glenn Hwy Suite 416</li>
-                               <li style={{listStyle:"none",fontWeight:"400",fontSize:"16px"}}> Dayton ,OH 45431</li></ul>
-                               </div>
-                               
-                               </div>
 
+                        <div className="conulli" style={{ display: "flex" }}>
+                            <div><CiLocationOn size={20} /> </div>
+                            <div style={{ marginTop: "-18px" }}> <ul style={{ display: "flex", flexDirection: "column", flexWrap: "wrap", textDecoration: "none" }}>
+                                <li style={{ listStyle: "none", fontWeight: "400", fontSize: "16px" }}>4031 Colonel Glenn Hwy Suite 416</li>
+                                <li style={{ listStyle: "none", fontWeight: "400", fontSize: "16px" }}> Dayton ,OH 45431</li></ul>
+                            </div>
 
-                        <div className="conulli" style={{ display:"flex"}}>
-                            <div><IoMailOutline /></div>
-                        <div style={{marginTop:"-18px"}}>   <ul><li style={{listStyle:"none",fontWeight:"400",fontSize:"16px"}}> info@tracs.app</li>
-                        </ul>
-                        </div> 
                         </div>
-                        <div  className="conulli" style={{display:"flex"}}> <div style={{marginTop:"0px"}}><FaPhoneAlt /></div>
-                          <div style={{marginTop:"-16px"}}> <ul>
-                                <li style={{listStyle:"none",fontWeight:"400",fontSize:"16px"}}> 513.371.5299</li>
-                            </ul></div> 
-                           
+
+
+                        <div className="conulli" style={{ display: "flex" }}>
+                            <div><IoMailOutline /></div>
+                            <div style={{ marginTop: "-18px" }}>   <ul><li style={{ listStyle: "none", fontWeight: "400", fontSize: "16px" }}> info@tracs.app</li>
+                            </ul>
+                            </div>
+                        </div>
+                        <div className="conulli" style={{ display: "flex" }}> <div style={{ marginTop: "0px" }}><FaPhoneAlt /></div>
+                            <div style={{ marginTop: "-16px" }}> <ul>
+                                <li style={{ listStyle: "none", fontWeight: "400", fontSize: "16px" }}> 513.371.5299</li>
+                            </ul></div>
+
                         </div>
 
 
@@ -93,10 +93,10 @@ const Footer = () => {
             </div>
             <div className='copyright-container'>
                 <div className='tpc-container'>
-                  <div style={{display:"flex",textAlign:"center",alignContent:'center',justifyContent:"center"}}>  <p><Link to='/terms' style={{textDecoration:"none",color:"inherit"}}>Terms and Condition</Link> / </p> <p><Link to='/privacy' style={{textDecoration:"none",color:"inherit"}}>Privact Policy</Link>  /</p> <p> <Link to='/contact' style={{textDecoration:"none",color:"inherit"}}> Contact</Link></p></div>
+                    <div style={{ display: "flex", textAlign: "center", alignContent: 'center', justifyContent: "center" }}>  <p><Link to='/terms' style={{ textDecoration: "none", color: "inherit" }}>Terms and Condition</Link> / </p> <p><Link to='/privacy' style={{ textDecoration: "none", color: "inherit" }}>Privact Policy</Link>  /</p> <p> <Link to='/contact' style={{ textDecoration: "none", color: "inherit" }}> Contact</Link></p></div>
                 </div>
                 <div>
-                    <h3 style={{color:"white"}}>Copyright 2025 .AptTech Solutions Inc . All Rights Reserved.</h3>
+                    <h3 style={{ color: "white" }}>Copyright 2025 .AptTech Solutions Inc . All Rights Reserved.</h3>
                 </div>
 
             </div>
